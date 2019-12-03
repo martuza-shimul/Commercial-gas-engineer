659d1b80e151a8d78d7b644dfb597ad47b65ef9f
Martuza <martuza.shimul@gmail.com>
2019-12-03 23:33:21 +0600

    js file added

---
+++ main.js
@@ -0,0 +1,6 @@
+// let scriptLink = `<script src="./main.js"></script>`;
+// document.querySelector(".footer").innerHTML += scriptLink;
+// document.getElementsByTagName("body").innerHTML += scriptLink;
+
+let logo = `<a class="navbar-brand" href="index.html"><img class="img-responsive" src="images/com-logo.png" alt="Logo"></a>`;
+document.querySelector(".navbar .navbar-brand").innerHTML = logo;
